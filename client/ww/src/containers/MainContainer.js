a79f3cd03825982200c4507654fc56df7ed7c063
Takako Onozuka <onozukatakako@gmail.com>
2023-06-08 16:15:52 +0100

    added maincontainer.js and maincontainer.css

---
+++ client/ww/src/containers/MainContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./MainContainer.css";
+
+const MainContainer = () => {
+    return ( 
+        <>
+        This is the main container
+        </>
+     );
+}
+ 
+export default MainContainer;
