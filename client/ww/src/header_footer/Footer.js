31f11d18e897104b3f627e00d2d1286da66d4bdc
Takako Onozuka <onozukatakako@gmail.com>
2023-06-09 10:28:55 +0100

    added footer text and styling

--- client/ww/src/header_footer/Footer.js
+++ client/ww/src/header_footer/Footer.js
@@ -1,12 +1,19 @@
+import React from "react";
+import "./headerfooter.css";
+
 const Footer = () => {
     return ( 
         <>
-        footer
+        <div class="footer">
+        <div class="eclipse"></div>    
+        <div class="footer-text">
+        Copyright@WisdomWorks All rights reserved. <br/>
+        This website is maintained by WisdomWorksTeam
+        
+        </div>
+        </div>
         </>
      );
 }
  
 export default Footer;
-<>
-footer
-</>
--- client/ww/src/header_footer/Header.js
+++ client/ww/src/header_footer/Header.js
@@ -1,3 +1,6 @@
+import React from "react";
+import "./headerfooter.css";
+
 const Header = () => {
     return ( 
         <>
